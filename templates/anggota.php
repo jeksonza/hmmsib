39ba188a765be3b09038b799a8c76dbdbdcd8022
Jekman <Jelirics@gmail.com>
2023-07-01 15:21:39 +0700

    first commit

---
+++ templates/anggota.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Organisasi Himpunan Mahasiswa MSIB - Anggota</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header>
+		<header>
+            <div class="logo">
+                <img src="logo.png" alt="Logo Himpunan Mahasiswa MSIB">
+                <h1>Himpunan Mahasiswa MSIB</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="event.html">Event</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="news.html">News</a></li>
+                    <li><a href="anggota.php">Anggota</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+	<main>
+		<section>
+			<h2>Daftar Anggota</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>Program Studi</th>
+						<th>Angkatan</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>John Doe</td>
+						<td>Manajemen Sumberdaya Perairan</td>
+						<td>2019</td>
+						<td>johndoe@example.com</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Jane Smith</td>
+						<td>Manajemen Sumberdaya Perairan</td>
+						<td>2020</td>
+						<td>janesmith@example.com</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>Bob Johnson</td>
+						<td>Manajemen Sumberdaya Perairan</td>
+						<td>2018</td>
+						<td>bjohnson@example.com</td>
+					</tr>
+					<!-- tambahkan baris lainnya sesuai dengan data anggota -->
+				</tbody>
+			</table>
+		</section>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 Himpunan Mahasiswa MSIB</p>
+	</footer>
+</body>
+</html>
